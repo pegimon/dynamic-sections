910b034b8996ed401e45d49d7d2bf0be6b939a72
yousef <67870537+meliodas227@users.noreply.github.com>
2020-10-31 11:45:23 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+this project is made to make a static styled html and css page a dynamic one by using javascript.
+i made sections interact with page when you scroll to the section as it go black by getting active class.
+in this project i made the links in the navbar roate you to the specified section by clicking it.
---
+++ js/app.js
@@ -0,0 +1,93 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+let theNav = document.querySelector('#navbar__list');
+let elem1;
+let elem2; 
+let sectionText = document.querySelector('#section1');
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+let list = document.querySelectorAll('section');
+list.forEach(element => {
+    elem1 = document.createElement('LI');
+    elem2 = document.createElement('A');
+    elem1.appendChild(elem2);
+    elem2.textContent = element.dataset.nav;
+    theNav.appendChild(elem1);
+    elem2.href = '#' + element.id;
+});
+
+// Add class 'active' to section when near top of viewport
+window.addEventListener('scroll',function(){
+    list.forEach(element => {
+        var sectionPositionT = element.getBoundingClientRect().top;
+        var sectionPositionB = element.getBoundingClientRect().bottom;
+       
+        if (sectionPositionT <= 1){
+            element.classList.add('your-active-class');
+        }
+        else if (sectionPositionB >= 1){
+            element.classList.remove('your-active-class');
+        }
+        if (sectionPositionB <= 1) {
+            element.classList.remove('your-active-class');
+        }
+    });
+});
+
+// Scroll to anchor ID using scrollTO event
+
+let links = document.querySelectorAll('a');
+list.forEach((item, index) => {
+    links[index].addEventListener('click', function () {
+        item.scrollIntoView({behavior:"smooth"});
+        item.classList.add('your-active-class');
+    });
+});
+
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
